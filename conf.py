0cb1c8d50ff21a12f09d09e22f6a9455d2c22cd7
paul <paul.szelesi@veeam.com>
2019-08-30 11:52:13 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -57,6 +57,6 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "haiku"
+html_theme = "classic"
 
 html_static_path = ['_static']
